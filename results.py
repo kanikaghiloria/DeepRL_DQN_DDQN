12c9c4e02f26456da69c4118d3d8dba74073aa06
KANIKA <kanika.ghiloria93@gmail.com>
2020-09-09 01:27:45 +0100

    visualisations

--- results.py
+++
@@ -1,8 +0,0 @@
-trainingResults = {
-    'AGENT':['DQN', 'DQN', 'DQN', 'DQN', 'DDQN', 'DDQN','DDQN','DDQN'],
-    'MODEL':['easy', 'medium', 'hard', 'general', 'easy', 'medium', 'hard', 'general'],
-    'MAXIMUM SCORE':[18720,10092,204,1016,28022,13136,1878,0],
-    'AVERAGE SCORE':[14530.5,4567.33,67.8,160.32,28022,5275.5,450.42,0],
-    'TRAINING TIME':[],
-}
-testingResults = {'AGENT':[], 'MODEL':[], 'MAXIMUM SCORE':[], 'AVERAGE SCORE':[],'TRAINING TIME':[]}
